c62c0d660b31772fe5448549061689947eaf72a9
Patrick Housley <patrick.f.housley@gmail.com>
2016-03-16 00:52:48 -0500

    Added tests for black card model. More tests to follow.

--- tests/server/models/blackcard.spec.js
+++ tests/server/models/blackcard.spec.js
@@ -1,7 +1,9 @@
 import {expect} from 'chai';
-import db from '../../../models/index';
+import database from '../../../models/index';
 
 describe('BlackCard Model', () => {
+
+  let db = database({ env: 'test' })
 
   it('should exist on the db object', () => {
     expect(db).to.not.be.null;
@@ -9,4 +11,44 @@ describe('BlackCard Model', () => {
     expect(db.BlackCard).to.not.be.null;
   });
 
-})
+  describe('Insert', () => {
+    let cardSet;
+
+    before((cb) => {
+      db.sequelize.sync({ force: true, logging: false }).then(() => {
+        db.CardSet.create({
+          active: true,
+          name: 'test',
+          base_deck: true,
+          description: 'test'
+        }, { logging: false }).then((res) => {
+          cardSet = res;
+          cb();
+        }).catch((err) => {
+          cb(err);
+        });
+      }).catch((err) => {
+        cb(err);
+      });
+    });
+
+    it('should be capable of inserting with minimal attributes', (cb) => {
+      db.BlackCard.create({
+        text: 'test',
+        card_set: cardSet
+      }, { logging: false }).then((blackCard) => {
+        expect(blackCard).to.not.be.null;
+        expect(blackCard.id).to.equal(1);
+        expect(blackCard.text).to.equal('test');
+        expect(blackCard.draw).to.equal(0);
+        expect(blackCard.pick).to.equal(1);
+        expect(blackCard.watermark).to.be.undefined;
+        cb();
+      }).catch((err) => {
+        cb(err);
+      });
+    });
+
+  });
+
+});
