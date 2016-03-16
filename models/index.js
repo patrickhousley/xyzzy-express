4679d0db176fd85fd7b5ac61e2d1bbb18da5c8c9
Patrick Housley <patrick.f.housley@gmail.com>
2016-03-16 00:50:35 -0500

    Changed model index to export a function allowing parameters to be passed in. This will be used to force a certain environment when running tests. Added environment value and config to returned db variable.

--- models/index.js
+++ models/index.js
@@ -2,36 +2,39 @@ import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
 
-let env = process.env.NODE_ENV || 'development';
-let config = require(__dirname + '/../config/config.json')[env];
-let sequelize;
-let db = {};
+export default function (options) {
+  let env = options.env || process.env.NODE_ENV || 'development';
+  let config = require(__dirname + '/../config/config.json')[env];
+  let sequelize;
+  let db = {};
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+  if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable]);
+  } else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
 
-fs.readdirSync(__dirname)
+  fs.readdirSync(__dirname)
   .filter((file) => {
     return file.indexOf('.') !== 0 &&
-      file !== path.basename(__filename) &&
-      file.slice(-3) === '.js';
+    file !== path.basename(__filename) &&
+    file.slice(-3) === '.js';
   })
   .forEach((file) => {
     let model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
-  console.log(modelName);
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+  Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+  db.env = env;
+  db.config = config;
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
 
-export default db;
+  return db;
+}
