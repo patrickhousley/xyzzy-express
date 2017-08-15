af328af87c519b7dde0ec61a5518ae3e4ea007a2
Patrick Housley <patrick.f.housley@gmail.com>
2017-08-15 00:44:29 -0500

    Adding back in the MaterialModule.

--- src/web/app/app.module.ts
+++ src/web/app/app.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
 import { AppRoutingModule } from 'src/web/app/app-routing.module';
 import { AppComponent } from 'src/web/app/app.component';
 import { SharedModule } from 'src/web/app/shared/shared.module';
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, SharedModule, HttpModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    SharedModule,
+    HttpModule,
+    AppRoutingModule
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
