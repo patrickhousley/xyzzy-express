e8581a308212a48767d3db974dc0e8d5d607bb7c
Patrick Housley <patrick.f.housley@gmail.com>
2017-08-14 22:37:08 -0500

    Correcting the layout so the header is above the sidenav and content.

--- src/web/app/app.component.ts
+++ src/web/app/app.component.ts
@@ -1,24 +1,48 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  OnInit,
+  ContentChild
+} from '@angular/core';
 import { ObservableMedia } from '@angular/flex-layout';
 import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { MdSidenav } from '@angular/material';
 
 @Component({
-  selector: 'xyzzy-root',
+  selector: 'xyzzy2-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ],
+  styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
-  public enableSideNav$: Observable<boolean>;
+  @ContentChild(MdSidenav) private sidenav: MdSidenav;
+  private openSidenavSubject = new BehaviorSubject<boolean>(false);
 
-  constructor(private media: ObservableMedia) {
-  }
+  public collapseSideNav$: Observable<boolean>;
+  public openSidenave$: Observable<boolean>;
+
+  constructor(private media: ObservableMedia) {}
 
   public ngOnInit() {
-    this.enableSideNav$ = this.media.asObservable()
-      .map(() =>
-        this.media.isActive('xs') || this.media.isActive('sm')
-      )
-      .startWith(this.media.isActive('xs') || this.media.isActive('sm'));
+    this.openSidenave$ = this.openSidenavSubject.asObservable();
+    this.collapseSideNav$ = this.media
+      .asObservable()
+      .map(() => this.media.isActive('xs') || this.media.isActive('sm'))
+      .startWith(this.media.isActive('xs') || this.media.isActive('sm'))
+      .do(isSmall => {
+        if (isSmall) {
+          // Reset the sidenav to be closed
+          this.openSidenavSubject.next(false);
+        }
+      });
+  }
+
+  public toggleSidenav() {
+    if (this.sidenav && !this.sidenav.opened) {
+      this.openSidenavSubject.next(true);
+    } else if (this.sidenav && this.sidenav.opened) {
+      this.openSidenavSubject.next(false);
+    }
   }
 }
